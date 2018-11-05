f30905c5693ef66f012b18856f3e00d9a9513718
ita <itafu123@gmail.com>
2018-11-05 23:55:52 +0800

    ita back v3

--- human.py
+++ human.py
@@ -88,3 +88,30 @@ class Human:
         else:
             return 0
 
+
+    #get initial back
+    def getInitialBack(self):
+        bh=math.pow((self.Neck[0]-self.MH[0]), 2)
+        bw=math.pow((self.Neck[1]-self.MH[1]), 2)
+        ib=math.sqrt(bh+bw)
+
+        return {'IB':ib}
+
+    #measure Back
+    def measureBack(self, ib):
+        
+         #Threshold
+        th=0.15 
+
+        nbh=math.pow((self.Neck[0]-self.MH[0]), 2)
+        nbw=math.pow((self.Neck[1]-self.MH[1]), 2)
+        nb=math.sqrt(nbh+nbw)
+
+        diff=abs(ib-nb)
+
+        if diff<th:
+            return 1
+        else:
+            return 0
+
+
