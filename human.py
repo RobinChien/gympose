cf025786f15cd423de3ca0a41bd6fe3b020b3dbb
RobinJian <goodnana1224@gmail.com>
2018-11-20 01:35:23 +0800

    Add measureBArch function

--- human.py
+++ human.py
@@ -33,26 +33,44 @@ class Human:
         self.LK = keypoints[0][13]
         #{14, "LAnkle"},!!
         self.LA = keypoints[0][14]
+        #{18, "LEar"},
+        self.LE = keypoints[0][18]
 
-        
+
+
     def getTArch(self):
         return np.array([self.Neck, self.RS, self.LS, self.MH])
-    
+
     def measureTArch(self, tarch):
         difArch = np.absolute(self.getTArch()-tarch)
         if difArch.any()>5:
             return 0
         return 1
 
-    #手腕有無超出腳踝
+    def getBArch(self):
+        print('gIB_Neck', self.Neck)
+        print('gIB_MH', self.MH)
+        bh=math.pow((self.Neck[0]-self.MH[0]), 2)
+        bw=math.pow((self.Neck[1]-self.MH[1]), 2)
+        print("gIB_bh", bh)
+        print("gIB_bw", bw)
+        ib=math.sqrt(bh+bw)
+        print("gIB_ib", ib)
+        return np.array([self.Neck, self.MH, self.LK])
+
+    def measureBArch(self, barch):
+        difArch = np.absolute(self.getBArch()-barch)
+        if difArch.any()>5:
+            return 0
+        return 1
+
     def measureWristsAndAnkles(self):
         hwidth = abs(self.RW[0]-self.LW[0])
         fwidth = abs(self.RA[0]-self.LA[0])
         if hwidth > fwidth:
             return 1
         return 0
-    
-    #肩膀有無和雙腳平行
+
     def measureShouldersAndAnleesParallel(self):
         soulder_ans = self.LS-self.RS
         soulder_result = float(soulder_ans[1])/float(soulder_ans[0])
@@ -60,9 +78,8 @@ class Human:
         anless_result = float(anlees_ans[1])/float(anlees_ans[0])
         if abs(soulder_result-anless_result)<0.15:
             return 1
-        return 0		
+        return 0
 
-    #雙腳間距有無超出肩膀寬度
     def measureShouldersAndAnkles(self):
         soulder_x = math.pow((self.RS[0]-self.LS[0]), 2)
         soulder_y = math.pow((self.RS[1]-self.LS[1]), 2)
@@ -75,37 +92,23 @@ class Human:
             return 1
         return 0
 
-    #膝蓋有沒有超出手
     def measureHandAndKnee(self):
         a=self.LW[1]-self.LS[1]
-        print("mHAK_a", a)
         b=self.LS[0]-self.LW[0]
-        print("mHAK_b", b)
         c=(self.LW[0]*self.LS[1])-(self.LS[0]*self.LW[1])
-        print("mHAK_c", c)
         e=math.sqrt((a*a)+(b*b))
-        print("mHAK_e", e)
         d=abs((a*self.LK[0])+(b*self.LK[1])+c)/e
-        print("mHAK_d", d)
         if self.LS[1] < self.LK[1] and self.LW[1] > self.LK[1]:
-            #print("1")
             if self.LS[0] > self.LK[0] and self.LW[0] > self.LK[0]:
-                #print("2")
                 if d>0:
-                    print(3)
                     return 1
-                else:
-                    return 0
-            else:
-                return 0
-        else:
-            return 0
-				
-    def measureArmAndBent(self):		
+        return 0
+
+    def measureArmAndBent(self):
         difSE = np.absolute(np.around(self.LS)-np.around(self.LE))
-        SE_results = float(difSE[0])/float(difSE[1])		
+        SE_results = float(difSE[0])/float(difSE[1])
         difEW = np.absolute(np.around(self.LE)-np.around(self.LW))
-        EW_results = float(difSE[0])/float(difEW[1])		
+        EW_results = float(difSE[0])/float(difEW[1])
         if abs(SE_results-EW_results)<0.2:
             return 1
         return 0
@@ -117,48 +120,72 @@ class Human:
         if (abs(mh-lk)<3):
             return 1
         return 0
-                
-    #get initial back
-    def getInitBack(self):
-        print('gIB_Neck', self.Neck)
-        print('gIB_MH', self.MH)
-        bh=math.pow((self.Neck[0]-self.MH[0]), 2)
-        bw=math.pow((self.Neck[1]-self.MH[1]), 2)
-        print("gIB_bh", bh)
-        print("gIB_bw", bw)
-        ib=math.sqrt(bh+bw)
-        print("gIB_ib", ib)
 
-        return np.array([[ib], self.Neck, self.MH, self.LK])
+    def measureRoundedShoulders(self):
+        longer_ans = self.LE-self.MH
+        longer_result = float(longer_ans[1])/float(longer_ans[0])
 
-
-    #measure Back
-    def measureBack(self, ib):
+        shorter_ans = self.LS-self.MH
+        shorter_result = float(shorter_ans[1])/float(shorter_ans[0])
         
-        #Threshold
-        th=10 
-
-        nbh=math.pow((self.Neck[0]-self.MH[0]), 2)
-        nbw=math.pow((self.Neck[1]-self.MH[1]), 2)
-        nb=math.sqrt(nbh+nbw)
-        
-        print("ib", ib)
-        diff=abs(ib-nb)
-        print("diff", diff)
-
-        if diff<th:
-            return 1
-        else:
+        if (longer_result<0) && (shorter_result<0):
+            if (longer_result-shorter_result)<=0:
+                return 1
             return 0
 
-    #measure if neck and bottom back at the same time
-    def measureNeckAndBottom(self, ineck, ibottom, iknee):
-        difNeck = abs(self.Neck[0]-ineck[0])
-        difBottom = abs(self.MH[0]-ibottom[0])
-        difKnee = abs(self.LK[0]-iknee[0])
-        print("mNAB_Neck", difNeck)
-        print("mNAB_Bottom", difBottom)
-        print("mNAB_Knee", difKnee)
-        if difNeck<3 and difBottom<3 and difKnee<3: #both back
+        if (longer_result>0) && (shorter_result>0):
+            if (longer_result-shorter_result)<=0:
+                return 1
+            return 0
+
+        if (longer_result>0) && (shorter_result<0):
+            if (longer_result-shorter_result)>=0:
+                return 1
+            return 0
+
+        if (longer_result<0) && (shorter_result>0):
+            if (longer_result-shorter_result)<=0:
+                return 1
+            return 0
+
+    # def getinitBack(self):
+    #     print('gIB_Neck', self.Neck)
+    #     print('gIB_MH', self.MH)
+    #     bh=math.pow((self.Neck[0]-self.MH[0]), 2)
+    #     bw=math.pow((self.Neck[1]-self.MH[1]), 2)
+    #     print("gIB_bh", bh)
+    #     print("gIB_bw", bw)
+    #     ib=math.sqrt(bh+bw)
+    #     print("gIB_ib", ib)
+    #     return np.array([[ib], self.Neck, self.MH, self.LK])
+
+    #measure Back
+    # def measureBack(self, ib):
+
+    #     #Threshold
+    #     th=10
+
+    #     nbh=math.pow((self.Neck[0]-self.MH[0]), 2)
+    #     nbw=math.pow((self.Neck[1]-self.MH[1]), 2)
+    #     nb=math.sqrt(nbh+nbw)
+
+    #     print("ib", ib)
+    #     diff=abs(ib-nb)
+    #     print("diff", diff)
+
+    #     if diff<th:
+    #         return 1
+    #     else:
+    #         return 0
+
+    def measureNeckAndBottom(self, barch_s, barch_e):
+        # difNeck = abs(self.Neck[0]-ineck[0])
+        # difBottom = abs(self.MH[0]-ibottom[0])
+        # difKnee = abs(self.LK[0]-iknee[0])
+        # print("mNAB_Neck", difNeck)
+        # print("mNAB_Bottom", difBottom)
+        # print("mNAB_Knee", difKnee)
+        difArch = np.absolute(barch_e-barch_s)
+        if difArch.all()<3: #both back
            return 1
         return 0
